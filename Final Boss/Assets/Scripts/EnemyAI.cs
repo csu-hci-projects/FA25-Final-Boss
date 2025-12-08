c40545c4d9824068ef4bd2092d71740583f6e27d
Macey <beardenm@colostate.edu>
2025-12-08 14:35:58 -0700

    Footsteps RAGHHH

--- Final Boss/Assets/Scripts/EnemyAI.cs
+++ Final Boss/Assets/Scripts/EnemyAI.cs
@@ -11,12 +11,18 @@ public class EnemyAI : MonoBehaviour
     public float patrolSpeed = 2f;
     public float chaseSpeed = 4f;
 
+    public AudioSource patrolAudio; 
+    public AudioSource chaseAudio; 
+
     private int waypointIndex = 0;
 
     void Start()
     {
         agent.speed = patrolSpeed;
         GoToNextWaypoint();
+
+        if (patrolAudio != null && !patrolAudio.isPlaying)
+            patrolAudio.Play();
     }
 
     void Update()
@@ -25,10 +31,23 @@ public class EnemyAI : MonoBehaviour
         {
             agent.speed = chaseSpeed;
             agent.SetDestination(player.position);
+
+            if (patrolAudio != null && patrolAudio.isPlaying)
+                patrolAudio.Stop();
+
+            if (chaseAudio != null && !chaseAudio.isPlaying)
+                chaseAudio.Play();
         }
         else
         {
             agent.speed = patrolSpeed;
+
+
+            if (chaseAudio != null && chaseAudio.isPlaying)
+                chaseAudio.Stop();
+
+            if (patrolAudio != null && !patrolAudio.isPlaying)
+                patrolAudio.Play();
 
             if (!agent.pathPending && agent.remainingDistance < 0.2f)
             {
