68faad9f2be6119637deaecaf1b44ee0ad9435c9
quinncatr <quintenjroberts@gmail.com>
2025-12-09 20:23:19 -0700

    Key Door and Lighting

---
+++ Final Boss/Assets/Scripts/Dooropen.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public float interactDistance = 3f;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+
+        if (dist <= interactDistance && Input.GetKeyDown(KeyCode.E))
+        {
+            if(Inventory.hasKey)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Door is locked.");
+            }
+        }
+    }
+}
