d87660277ff5e9a5f3f7fc3aa94453418884a0df
quinncatr <quintenjroberts@gmail.com>
2025-12-10 14:54:49 -0700

    everything

--- Final Boss/Assets/Scripts/Dooropen.cs
+++ Final Boss/Assets/Scripts/Dooropen.cs
@@ -19,6 +19,8 @@ public class DoorOpen : MonoBehaviour
             if(Inventory.hasKey)
             {
                 Destroy(gameObject);
+                Inventory.hasKey = false;
+                KeyUI.instance.ShowKeyIcon(false);
             }
             else
             {
